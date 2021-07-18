ee57fe685f0f03619faff0fb6c0e9d57a936c7d5
Takkun <takkun.053@gmail.com>
2021-07-19 07:48:03 +0900

    バグの修正+座標表示機能の追加

--- js/main.js
+++ js/main.js
@@ -6,19 +6,7 @@ var map_x = 0;
 var map_y = 0;
 var mouse_x;
 var mouse_y;
-
-
-// data.json
-var xhr = new XMLHttpRequest();
-xhr.responseType = "json";
-xhr.onload = function() {
-    data = xhr.response;
-    init();
-    draw();
-};
-
-xhr.open("GET", "data.json");
-xhr.send();
+var mouse_drag;
 
 
 // 関数
@@ -26,26 +14,25 @@ function init() {
     canvas.width = document.body.clientWidth;
     canvas.height = document.body.clientHeight;
     ctx.translate(document.body.clientWidth / 2, document.body.clientHeight / 2);
+
     ctx.lineWidth = 5;
     ctx.font = "0.75rem sans-serif";
     ctx.textAlign = "center";
 };
 
-
 function draw() {
     ctx.clearRect(canvas.width / 2 * -1, canvas.height / 2 * -1, canvas.width, canvas.height);
+    
     var stations = [];
     for (d1 in data) {
         CompanyData = data[d1];
         ctx.fillStyle = CompanyData["color"];
         ctx.strokeStyle = CompanyData["color"];
         linesData = CompanyData["point"];
-
         for (d2 in linesData) {
             routeData = linesData[d2];
             ctx.beginPath();
             ctx.moveTo(map_x + routeData[0]["coord"][0], map_y + routeData[0]["coord"][1]);
-
             for (d3 in routeData) {
                 pointData = routeData[d3];
                 coord = pointData["coord"];
@@ -61,28 +48,48 @@ function draw() {
             ctx.stroke();
         };
     };
+
     ctx.fillStyle = "#000";
-    for (d1 in stations) {
-        ctx.fillText(stations[d1][0], stations[d1][1], stations[d1][2]);
+    for (s1 in stations) {
+        ctx.fillText(stations[s1][0], stations[s1][1], stations[s1][2]);
     };
 };
 
 
 // イベント
 onload = () => {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = "json";
+    xhr.onload = () => {
+        data = xhr.response;
+        init();
+        draw();
+    };
+    xhr.open("GET", "data.json");
+    xhr.send();
+
     canvas = document.getElementById("main");
     ctx = canvas.getContext("2d");
+    
     canvas.onmousedown = e => {
         mouse_x = e.offsetX;
         mouse_y = e.offsetY;
-        canvas.onmousemove = e => {
+        mouse_drag = true;
+    };
+    document.onmouseup = () => mouse_drag = false;
+    canvas.onmousemove = e => {
+        if (mouse_drag) {
             map_x += e.offsetX - mouse_x;
             map_y += e.offsetY - mouse_y;
             draw();
             mouse_x = e.offsetX;
             mouse_y = e.offsetY;
         };
+        document.getElementsByClassName("coord")[0].innerText = `${map_x * -1 + e.offsetX - document.body.clientWidth / 2}, ${map_y * -1 + e.offsetY - document.body.clientHeight / 2}`;
     };
-    canvas.onmouseup = () => canvas.onmousemove = null;
 };
-onresize = init;
+
+onresize = () => {
+    init();
+    draw();
+};
